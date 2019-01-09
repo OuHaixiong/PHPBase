e68d416cc63a127a8eb21ca0df5048134c000953
Bear <258333309@qq.com>
2019-01-09 14:19:34 +0800

    对称、非对称加解密

--- encrypt.php
+++ encrypt.php
@@ -1,5 +1,6 @@
 <?php
-// 下面演示字符串的加密和解密
+// http://www.cnblogs.com/JeffreySun/archive/2010/06/24/1627247.html 这文章讲的比较详细，有些原理和方法可以直接在代码中应用
+// 下面演示字符串的加密和解密（对称加密）。 对称加密的常用算法有: DES算法(貌似有安全问题)，3DES算法，TDEA算法，Blowfish算法，RC5算法，IDEA算法，AES。
 $aString = '我是欧海雄';
 $aEncodeString = base64_encode($aString); // 对字符串进行base64编码
 var_dump($aEncodeString); // 5oiR5piv5qyn5rW36ZuE
@@ -22,11 +23,12 @@ var_dump($bDecodeString);
 // print_r('++++++++++++++');
 
 print_r('-----------------' . "\n");
-$data = '我是欧阳海雄';
-$password = 'abc123';
-$iv = '3B65571F4EB0F92a'; // 必须为16位的字符串
+$data = '我是欧阳海雄'; // 需加密的数据
+$password = 'abc123'; // 加密密钥（key、salt），最长为16位 （AES-128-CBC为16位，AES-256-CBC为32位）
+$iv = '3B65571F4EB0F92a'; // 必须为16位的字符串  【初始化向量（IV）】
 $options = OPENSSL_RAW_DATA;
-$method = 'AES-128-CBC';
+$method = 'AES-128-CBC'; // AES-256-ECB：这个模式不推荐，容易遭受字典攻击 。 AES : 高级加密标准Advanced Encryption Standard
+// 推荐使用AES-256-CBC
 $encryptString = openssl_encrypt($data, $method, $password, $options, $iv); // php7中建议用openssl_*系列的函数代替mcrypt_*
 var_dump($encryptString); // string(32) "��Y�ϭ�N�n��;�b�`N��������
 $decryptString = openssl_decrypt($encryptString, $method, $password, $options, $iv); // string(18) "我是欧阳海雄"
@@ -34,9 +36,93 @@ var_dump($decryptString);
 print_r('-----------------' . "\n");
 
 
+// 下面是discuz的加密和解密函数
+/**
+ * discuz的加密和解密函数
+ * @param string $string 需要加解密的字符串
+ * @param string $operation 操作； DECODE：解密， ENCODE：加密
+ * @param string $key 密钥
+ * @param number $expiry 过期时间，单位为秒。如果是解密可以不传。0为永不过期
+ * @return string 同样的字符串，每次加密出来都不一样
+ */
+function authcode($string, $operation = 'DECODE', $key = '', $expiry = 0) {
+    // 动态密匙长度，相同的明文会生成不同密文就是依靠动态密匙
+    $ckey_length = 4;
+    $key = md5($key); // 密匙
+    // 密匙a会参与加解密
+    $keya = md5(substr($key, 0, 16));
+    // 密匙b会用来做数据完整性验证
+    $keyb = md5(substr($key, 16, 16));
+    // 密匙c用于变化生成的密文
+    $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length):
+        substr(md5(microtime()), -$ckey_length)) : '';
+    // 参与运算的密匙
+    $cryptkey = $keya.md5($keya.$keyc);
+    $key_length = strlen($cryptkey);
+    // 明文，前10位用来保存时间戳，解密时验证数据有效性，10到26位用来保存$keyb(密匙b)，
+    //解密时会通过这个密匙验证数据完整性
+    // 如果是解码的话，会从第$ckey_length位开始，因为密文前$ckey_length位保存 动态密匙，以保证解密正确
+    $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) :
+    sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;
+    $string_length = strlen($string);
+    $result = '';
+    $box = range(0, 255);
+    $rndkey = array();
+    // 产生密匙簿
+    for($i = 0; $i <= 255; $i++) {
+        $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+    }
+    // 用固定的算法，打乱密匙簿，增加随机性，好像很复杂，实际上对并不会增加密文的强度
+    for($j = $i = 0; $i < 256; $i++) {
+        $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+        $tmp = $box[$i];
+        $box[$i] = $box[$j];
+        $box[$j] = $tmp;
+    }
+    // 核心加解密部分
+    for($a = $j = $i = 0; $i < $string_length; $i++) {
+        $a = ($a + 1) % 256;
+        $j = ($j + $box[$a]) % 256;
+        $tmp = $box[$a];
+        $box[$a] = $box[$j];
+        $box[$j] = $tmp;
+        // 从密匙簿得出密匙进行异或，再转成字符
+        $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+    }
+    if($operation == 'DECODE') {
+        // 验证数据有效性，请看未加密明文的格式
+        if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) &&
+            substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
+                return substr($result, 26);
+            } else {
+                return '';
+            }
+    } else {
+        // 把动态密匙保存在密文里，这也是为什么同样的明文，生产不同密文后能解密的原因
+        // 因为加密后的密文可能是一些特殊字符，复制过程可能会丢失，所以用base64编码
+        return $keyc.str_replace('=', '', base64_encode($result));
+    }
+}
+echo('<br />++++++++<br />');
+$string = '我是欧阳海雄OK!';
+$operation = 'ENCODE';
+$key = 'abc123';
+// $expiry = 30;
+// $encodeString = authcode($string, $operation, $key, $expiry);
+// echo 'discuz加密，“' . $string . '”， 加密后的字符串为： ' . $encodeString;
+echo '<br />';
+$operation = 'DECODE';
+// $string = $encodeString;
+// $string = '9d25mq6V88/vPHERg0tADmWYTRLl4c4EMnIeJ/jnXPEiakwJrgv81pj+4+YrtmxAFWY';
+// $string = '5a82NZBTZZR+seIIPBRXJlqiFi1xweBgDAswBi9CcpDD1RQHEX4gIfKNlYee/umm4pI';
+// $string = '0e32YPGHDyumNKi8Vok9uplxVtV0r2I8VyuSFlevtV0I5Bec91QE2ewUDLu5ZRTB7/M';
+$string = 'f7d60p+gL6XswirXp2YBZo6p8MSoZmKSh7fDm3apje1ABG4koc6iRv0IJw19ME8undU';
+$decodeString = authcode($string, $operation, $key);
+echo 'discuz解密后的字符串为： ' . $decodeString;
+echo '<br />++++++++<br />';
 
 
-// 下面演示不可逆的加密算法
+// 下面演示不可逆的加密算法(摘要算法、散列函数或哈希函数)
 $cString = 'zhong文，sdf123';
 $encodeString = sha1($cString);
 var_dump($encodeString); // string(40) "ec08ace04cef7b56d33bbb0c2dc50b7584a11bb8"
